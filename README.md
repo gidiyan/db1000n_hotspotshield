8293c943ca7f71d06a49e5d596352a3799741f9a
gidiyan <gidiyan@gmail.com>
2022-03-29 21:55:21 +0300

    commit 0bb868f77e5d1f394e660a62dd1adca902cfe779 Author: gidiyan <gidiyan@gmail.com> Date:   Tue Mar 29 20:31:46 2022 +0300
        Update README with info about db1000n from their repository
    
    commit b0a9979204daf985f780cafec7534939c5e288fe
    Author: gidiyan <gidiyan@gmail.com>
    Date:   Tue Mar 29 20:28:29 2022 +0300
    
        Add proxy usage condition
    
    commit a1ce116661f3f8bdd5d83c897a7a66292b1235e7
    Author: gidiyan <gidiyan@gmail.com>
    Date:   Tue Mar 29 19:59:20 2022 +0300
    
        Change script name
    
    commit 9faeb005b16c638294175265cb64c78715bc7591
    Author: gidiyan <gidiyan@gmail.com>
    Date:   Tue Mar 29 19:43:58 2022 +0300
    
        Add script
    
    commit 9d6fbde1fb5cfae327c263b5d36efaf1be3a42a7
    Author: gidiyan <70430920+gidiyan@users.noreply.github.com>
    Date:   Tue Mar 29 16:34:29 2022 +0000
    
        Initial commit

---
+++ README.md
@@ -0,0 +1,14 @@
+# Death by 1000 needles automaticly using hotspotshield
+
+See [Docs](https://arriven.github.io/db1000n)
+
+This is a simple distributed load generation client written in go.
+It is able to fetch simple json config from a local or remote location.
+The config describes which load generation jobs should be launched in parallel.
+There are other existing tools doing the same kind of job.
+I do not intend to copy or replace them but rather provide a simple open source alternative so that users have more options.
+Feel free to use it in your load tests (wink-wink).
+
+The software is provided as is under no guarantee.
+
+[Gitlab mirror](https://gitlab.com/db1000n/db1000n.git)
---
+++ db1000_hotspotshield.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+
+#script requires installed and configured hotspotshield VPN client
+#для роботи скрипту необхідний встановленний та налаштований hotspotshield VPN
+
+#script for automatic download newest version of db1000n and running it only if hotspotshield is active
+#you can choose whatever server to connect to. to change server change RU to another country from country list availibaly by commend hotspotshield locations
+#скрипт для автоматичного встановлення останньої версії db1000n та запуску тільки якщо hotspotshield під'єднаний до сервера.список локацій доступний по команді hotspotshield locations
+#сервер можете вибрати будь-який,але найефективніше працювати з Росії. 
+location=RU
+
+#to get info that you are still connected to VPN server leave the line below without changes. else change to false
+#щоб отримувати інформацію про стан з'єднання з VPN, залиште значення нижче без змін.щоб вимкнути вивід про стан підключення, змініть значення на false
+connected=true #true are false 
+
+#timing between checks if still connected to VPN server.put number in seconds
+#як часто перевіряти з'єднання з VPN сервером.час в секундах
+timing=10s
+
+#if you don't want to use proxy leave the line unchanged. for using proxy change value to true
+#якщо не хочете використовувати проксі, залиште значення без змін. для використання проксі змініть на true
+use_proxy=false
+
+#script
+source <(curl https://raw.githubusercontent.com/Arriven/db1000n/main/install.sh)
+
+while true
+do
+	if hotspotshield status | grep -q 'disconnected'
+		then 
+			if pgrep db1000n 
+			then
+				tput setaf 1;echo "disconnected from hotspotshield server";echo "killing db1000n to restart connection to hotspotshield";tput setaf 6;\
+				pgrep -f db1000n | xargs kill -9; sleep 2s;  \
+			fi 	
+				tput setaf 1;echo "$(date +%T) connecting ru vpn";hotspotshield connect $location; sleep 5s; \
+				echo "starting new instance db1000n";tput setaf 6; \
+				if $use_proxy
+				then
+					db1000n --proxy '{{ join (split (get_url "https://raw.githubusercontent.com/porthole-ascend-cinnamon/proxy_scraper/main/proxies.txt") "\n") "," }}'& 
+				else
+					db1000n&
+				fi
+
+else
+			if $connected
+			then
+				tput setaf 1;echo "$(date +%T) hotspotshield still active";tput setaf 6; sleep  $timing
+			else 
+				sleep $timing
+			fi
+	fi	
+done
